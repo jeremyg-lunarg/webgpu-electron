9a6f91e6fe99cf2060db7b57dd94ba57a595066b
Jeremy Gebben <jeremyg@lunarg.com>
2024-08-29 13:03:50 -0600

    triangle

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-const { app, BrowserWindow } = require('electron/main')
-
+const { app, BrowserWindow, crashReporter } = require('electron/main')
+crashReporter.start({uploadToServer: false})
 app.commandLine.appendSwitch("enable-features", "Vulkan")
 app.commandLine.appendSwitch("enable-unsafe-webgpu")
 app.commandLine.appendSwitch("enable-webgpu-developer-features")
 const createWindow = () => {
   const win = new BrowserWindow({
-    height: 560,
-    width: 540,
+      fullscreen: true,
   });
   win.webContents.openDevTools()
-  win.loadFile('life.html')
+  win.loadFile('triangle.html')
 }
 
 app.whenReady().then(() => {
